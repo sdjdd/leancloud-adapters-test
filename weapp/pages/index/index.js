c16a971aee4cd4e741804f2cc286c1e4e63e6222
sdjdd <i@sdjdd.com>
2020-07-29 13:16:02 +0800

    feat(weapp): test Storage

--- weapp/pages/index/index.js
+++ weapp/pages/index/index.js
@@ -9,6 +9,37 @@ AV.init({
   serverURL: ''
 })
 
+Page({
+  data: {},
+  testLiveQuery() {
+    wx.showLoading({ title: '正在测试 LiveQuery' })
+    testLiveQuery().then(() => {
+      wx.hideLoading()
+      wx.showToast({ title: '测试成功' })
+    }).catch(() => {
+      wx.hideLoading()
+      wx.showToast({ title: '测试失败', icon: 'none' })
+    })
+  },
+  testUploadFile() {
+    wx.showLoading({ title: '正在上传文件' })
+    testUploadFile().then(() => {
+      wx.hideLoading()
+      wx.showToast({ title: '上传成功' })
+    }).catch(() => {
+      wx.hideLoading()
+      wx.showToast({ title: '上传失败', icon: 'none' })
+    })
+  },
+  testStorage() {
+    testStorage().then(() => {
+      wx.showToast({ title: 'Storage ok' })
+    }).catch(() => {
+      wx.showToast({ title: 'Storage error', icon: 'none' })
+    })
+  }
+})
+
 async function testLiveQuery() {
   let test = new AV.Object('Test')
   test.set('str', 'test weapp adapters')
@@ -57,26 +88,20 @@ async function testUploadFile() {
   file.destroy()
 }
 
-Page({
-  data: {},
-  testLiveQuery() {
-    wx.showLoading({ title: '正在测试 LiveQuery' })
-    testLiveQuery().then(() => {
-      wx.hideLoading()
-      wx.showToast({ title: '测试成功' })
-    }).catch(() => {
-      wx.hideLoading()
-      wx.showToast({ title: '测试失败', icon: 'none' })
-    })
-  },
-  testUploadFile() {
-    wx.showLoading({ title: '正在上传文件' })
-    testUploadFile().then(() => {
-      wx.hideLoading()
-      wx.showToast({ title: '上传成功' })
-    }).catch(() => {
-      wx.hideLoading()
-      wx.showToast({ title: '上传失败', icon: 'none' })
-    })
+async function testStorage() {
+  const storage = adapters.storage
+  storage.setItem('test', 'test')
+  if (storage.getItem('test') !== 'test') {
+    throw 'value not correct'
   }
-})
+  storage.removeItem('test')
+  if (storage.getItem('test')) {
+    throw 'value not removed'
+  }
+  storage.setItem('test1', 'test1')
+  storage.setItem('test2', 'test2')
+  storage.clear()
+  if (storage.getItem('test1') || storage.getItem('test2')) {
+    throw 'storage not cleared'
+  }
+}
